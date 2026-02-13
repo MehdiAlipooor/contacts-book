f0895dff670567359c9be10ae035251c2252cf66
MehdiAlipooor <alipoordev6@gmail.com>
2026-02-13 19:13:35 +0330

    proccess.exit(0) implemented, so after user press ctrl+c, an ugly error wont be shown in console

--- src/lib/createCliModule/index.ts
+++ src/lib/createCliModule/index.ts
@@ -4,45 +4,52 @@ import { commands } from "./commands";
 import type { CliModuleConfig, PromptDefinition } from "./types";
 
 async function executePrompt(
-	prompt: PromptDefinition,
-): Promise<{ key: string; value: string }> | never {
-	const { type, key } = prompt;
+  prompt: PromptDefinition,
+): Promise<{ key: string; value: string } | undefined> {
+  const { type, key } = prompt;
 
-	if (type === "input") {
-		const { message, validator, theme } = prompt;
-		const value = await commands.input({ message, validator, theme });
-		return { key, value };
-	}
+  if (type === "input") {
+    const { message, validator, theme } = prompt;
+    const value = await commands.input({ message, validator, theme });
+    return { key, value };
+  }
 
-	if (type === "select") {
-		const { message, choices } = prompt;
-		const value = await commands.select({ message, choices });
-		return { key, value };
-	}
+  if (type === "select") {
+    const { message, choices } = prompt;
+    const value = await commands.select({ message, choices });
+    return { key, value };
+  }
 
-	if (type === "search") {
-		const { message, onSearch } = prompt;
-		const value = await commands.search({ message, onSearch });
-		return { key, value };
-	}
+  if (type === "search") {
+    const { message, onSearch } = prompt;
+    const value = await commands.search({ message, onSearch });
+    return { key, value };
+  }
 
-	throw new ExitPromptException(`Unknown prompt type`);
+  console.log("user got out");
 }
 
 export const createCliModule = async (
-	config: CliModuleConfig,
+  config: CliModuleConfig,
 ): Promise<void> => {
-	const answers: Record<string, string> = {};
+  const answers: Record<string, string> = {};
 
-	for (const prompt of config.prompts) {
-		try {
-			const { key, value } = await executePrompt(prompt);
-			answers[key] = value;
-		} catch (error) {
-			handleError(error);
-			throw error;
-		}
-	}
+  for (const prompt of config.prompts) {
+    try {
+      const response = await executePrompt(prompt);
+      if (!response) {
+        throw new ExitPromptException("No prompt");
+      }
+      const { key, value } = response as {
+        key: string;
+        value: string;
+      };
+      answers[key] = value;
+    } catch (error) {
+      handleError(error);
+      process.exit(0);
+    }
+  }
 
-	await config.action(answers);
+  await config.action(answers);
 };
