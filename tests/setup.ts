8c8c4850d5283bccca7cf8063554864215fbbbd9
MehdiAlipooor <alipoordev6@gmail.com>
2026-02-14 17:10:26 +0330

    setup test file updated

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,17 +1,21 @@
 import { jest } from "@jest/globals";
 
+/**
+ * @default 5000
+ * @description Maybe a test was longer(like async operations)
+ */
 jest.setTimeout(10000);
 
 global.console = {
-	...console,
-	log: jest.fn(),
-	error: jest.fn(),
-	warn: jest.fn(),
-	info: jest.fn(),
-	debug: jest.fn(),
+  ...console,
+  log: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
 };
 
 afterEach(() => {
-	jest.clearAllMocks();
-	jest.resetAllMocks();
+  jest.clearAllMocks();
+  jest.resetAllMocks();
 });
